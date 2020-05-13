4f3f844979988ee41a6da7c8c8ae808c2e80d6f9
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-13 19:58:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.replace(/\b[a-zA-Z]/gi, x => x.toUpperCase() )
+
+  }
+
+  static sanitize(str){
+    return str.replace(/[^a-z\s\'\"\-]/gi,"")
+  }
+
+  static titleize(str){
+    const exceptions = ['the','a','an','but','of','and','for','at','by','from']
+    let newStr = str.charAt(0).toUpperCase() + str.slice(1);
+    return newStr.replace(/\b(\w*)/gim, function(x){
+      return (exceptions.includes(x)) ? x : (x.charAt(0).toUpperCase() + x.slice(1))
+    });
+  }
 }
