f4b04ccc95815cc2263734ff29394e15fc398453
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-13 19:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.replace(/\b[a-zA-Z]/gi, x => x.toUpperCase() )
+
+  }
+
+  static sanitize(str){
+    return str.replace(/[^a-z\s\'\"\-]/gi,"")
+  }
+
+  static titleize(string){
+    const exceptions = ['the','a','an','but','of','and','for','at','by','from']
+    let newstring = string.charAt(0).toUpperCase() + string.slice(1);
+    return newstring.replace(/\b(\w*)/gim, function(x){ 
+      return (exceptionalWords.includes(x)) ? x : (x.charAt(0).toUpperCase() + x.slice(1)) 
+    });
+  }
 }
